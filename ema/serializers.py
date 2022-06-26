945b894c824a8500b3863f8cb97988a64018ff9b
abdulchy2022 <102652685+abdulchy2022@users.noreply.github.com>
2022-06-26 20:38:24 +0100

    Delete ema directory

--- ema/serializers.py
+++
@@ -1,9 +0,0 @@
-from operator import itemgetter
-from pyexpat import model
-from rest_framework import serializers
-from ema.models  import Customer
-
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = '__all__' 
